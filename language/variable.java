3ca6c1f26b3db106ba9e23d9b8c4067e5973a42e
Art Navsegda <artnavsegda@gmail.com>
2019-07-01 05:17:58 +0300

    language

---
+++ language/variable.java
@@ -0,0 +1,18 @@
+public class hello {
+
+	public static void main(String[] args) {
+		int i;
+		i = 42;
+		System.out.println(i);
+		float x;
+		x = 26.32f;
+		System.out.println(x);
+		String f;
+		f = "32";
+		System.out.println(f);
+		char y;
+		y = 'x';
+		System.out.println(y);
+	}
+}
+
