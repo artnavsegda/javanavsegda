e407fec69fec9194cdd393bb48d2f0f2e6c24860
Art Navsegda <artnavsegda@gmail.com>
2019-07-01 13:03:51 +0300

    Create operators.java

---
+++ language/operators.java
@@ -0,0 +1,33 @@
+public class hello {
+
+	public static void main(String[] args) {
+		int i;
+		i = 42 + 20;
+		System.out.println(i);
+		i++;
+		System.out.println(i);
+		--i;
+		System.out.println(i);
+		i = 42 - 20;
+		System.out.println(i);
+		i = 42 * 2;
+		System.out.println(i);
+		i = 42 / 2;
+		System.out.println(i);
+		i = 5 % 3;
+		System.out.println(i);
+		float x;
+		x = 26.32f + 10.0f;
+		System.out.println(x);
+		x = 26.32f - 10.0f;
+		System.out.println(x);
+		x = 13f * 1.5f;
+		System.out.println(x);
+		x = 13f / 2f;
+		System.out.println(x);
+		String f;
+		f = "hello" + "world";
+		System.out.println(f);
+	}
+}
+
