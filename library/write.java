330cbfebf5ba3f586fd17ebe28d434a2b8586fb7
Art Navsegda <artnavsegda@gmail.com>
2019-07-12 15:14:05 +0300

    Update write.java

--- library/write.java
+++ library/write.java
@@ -3,14 +3,11 @@ import java.io.IOException;
 
 public class write {
 
-	public static void main(String[] args) {
-		try {
+	public static void main(String[] args) 
+		throws IOException {
 			FileWriter myfile = new FileWriter("test.txt");
 			myfile.write("Hello World");
 			myfile.close();
-		} catch (IOException ex) {
-			ex.printStackTrace();
 		}
-	}
 }
 
