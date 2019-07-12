2740a06c842ea69f7a82f74c90276a2d5c70bf7f
Art Navsegda <artnavsegda@gmail.com>
2019-07-12 16:40:11 +0300

    udobno

--- library/read.java
+++ library/read.java
@@ -4,7 +4,7 @@ public class write {
 
 	public static void main(String[] args) throws Exception
 	{
-		FileReader myfile = new FileReader("test.txt");
+		FileReader myfile = new FileReader("test2.txt");
 		int i;
 		while ((i=myfile.read()) != -1)
 			System.out.print((char) i);
--- library/rename.java
+++ library/rename.java
@@ -3,8 +3,8 @@ import java.io.File;
 public class delete {
 
 	public static void main(String[] args) {
-		File myfile = new File("test2.txt");
-		myfile.delete();
+		File myfile = new File("test.txt");
+		myfile.renameTo(new File("test2.txt"));
 	}
 }
 
