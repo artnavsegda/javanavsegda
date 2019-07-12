52c4c92b3a0973764517f90f4b05173b030fbd9a
Art Navsegda <artnavsegda@gmail.com>
2019-07-12 21:45:41 +0300

    new move

--- library/sockets/server.java
+++ library/sockets/server.java
@@ -1,9 +1,12 @@
+import java.net.Socket;
 import java.net.ServerSocket;
+import java.io.PrintWriter;
 
 public class server {
 	public static void main(String[] args) throws Exception
 	{
 		ServerSocket mysocket = new ServerSocket(8888);
+		Socket othersocket = mysocket.accept();
+		PrintWriter out = new PrintWriter(othersocket.getOutputStream(), true);
 	}
 }
-
