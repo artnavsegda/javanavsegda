3083bf3cb63b8bee8038bc10ac63624e5660e0d0
Art Navsegda <artnavsegda@gmail.com>
2019-07-13 22:37:31 +0300

    cast

---
+++ language/cast.java
@@ -0,0 +1,9 @@
+public class hello {
+
+	public static void main(String[] args)
+	{
+		float x = 26.32f;
+		System.out.println((int)x);
+	}
+}
+
--- language/variable.java
+++ language/variable.java
@@ -1,6 +1,7 @@
 public class hello {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		boolean a;
 		a = true;
 		System.out.println(a);
