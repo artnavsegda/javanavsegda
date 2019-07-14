a3b89394561ad46e4a88c10ab2d421d349f9456b
Art Navsegda <artnavsegda@gmail.com>
2019-07-14 20:05:48 +0300

    Create exception.java

---
+++ language/exception.java
@@ -0,0 +1,16 @@
+public class method {
+	static void myfunc2(int number)
+	{
+		System.out.println(number);
+	}
+	static void myfunc(int number)
+	{
+		System.out.println("Hello, World");
+	}
+	public static void main(String[] args)
+	{
+		myfunc(1);
+		myfunc2(2);
+	}
+}
+
