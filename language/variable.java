c1fb31ed07934c4b55c23f01357ca33917c037d4
Art Navsegda <artnavsegda@gmail.com>
2019-07-14 04:26:12 +0300

    Update variable.java

--- language/variable.java
+++ language/variable.java
@@ -6,20 +6,32 @@ public class hello {
 		a = true;
 		System.out.println(a);
 		int i;
-		i = 42;
+		i = 400000;
 		System.out.println(i);
+		short s;
+		s = 5000;
+		System.out.println(s);
+		long l;
+		l = 150000000000L;
+		System.out.println(l);
 		float x;
 		x = 26.32f;
 		System.out.println(x);
-		String f;
-		f = "32";
-		System.out.println(f);
+		double d;
+		d = 19.99d;
+		System.out.println(d);
 		char y;
 		y = 'x';
 		System.out.println(y);
+		byte b;
+		b = 100;
+		System.out.println(b);
+		String f;
+		f = "32";
+		System.out.println(f);
 		var o = 24;
 		System.out.println(o);
-		final int r = 103;// const
+		final int r = 234313;// const
 		System.out.println(r);
 	}
 }
