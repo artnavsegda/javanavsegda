4707eea693917621548e9588373b3fbfdb168794
Art Navsegda <artnavsegda@gmail.com>
2019-07-14 05:26:41 +0300

    Create string.java

---
+++ library/string/string.java
@@ -0,0 +1,10 @@
+public class string {
+	public static void main(String[] args)
+	{
+		String str;
+		str = "hello" + "world";
+		System.out.println(str);
+		System.out.println(str.length());
+	}
+}
+
