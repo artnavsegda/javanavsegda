8a0ca92d7cc4604b2dd6f30a368f55f8311f4f85
Art Navsegda <artnavsegda@gmail.com>
2019-07-17 22:26:46 +0300

    Create constructors.java

---
+++ language/class/constructors.java
@@ -0,0 +1,16 @@
+public class main
+{
+	public static void main(String[] args)
+	{
+		myclass myobj = new myclass();
+	}
+}
+
+public class myclass
+{
+	public myclass()
+	{
+		System.out.println("Class created");
+	}
+}
+
