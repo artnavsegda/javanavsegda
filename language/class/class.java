4676b357301575aa90ba454aab9ace530c88d355
Art Navsegda <artnavsegda@gmail.com>
2019-07-17 22:09:24 +0300

    Create class.java

---
+++ language/class/class.java
@@ -0,0 +1,15 @@
+public class main
+{
+	public static void main(String[] args)
+	{
+		myclass myobj = new myclass();
+		myobj.i = 10;
+		System.out.println(myobj.i);
+	}
+}
+
+public class myclass
+{
+	int i;
+}
+
