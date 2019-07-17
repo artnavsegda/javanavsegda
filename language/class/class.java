e06771567e741bdffda0a7cbab74c2b253b651db
Art Navsegda <artnavsegda@gmail.com>
2019-07-17 22:15:56 +0300

    Update class.java

--- language/class/class.java
+++ language/class/class.java
@@ -5,11 +5,16 @@ public class main
 		myclass myobj = new myclass();
 		myobj.i = 10;
 		System.out.println(myobj.i);
+		myobj.printi();
 	}
 }
 
 public class myclass
 {
 	int i;
+	static void printi()
+	{
+		System.out.println(11);
+	}
 }
 
