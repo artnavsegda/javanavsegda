d6b6793a7d78b8d23a0eb72ccb00e8ac122f85ab
Art Navsegda <artnavsegda@gmail.com>
2019-07-20 13:45:42 +0300

    scanner

---
+++ library/scanner/scanner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class hello
+{
+	public static void main(String[] args)
+	{
+		Scanner myscan = new Scanner(System.in);
+		String mystring = myscan.nextLine();
+		System.out.println(mystring);
+	}
+}
+
