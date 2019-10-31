af5d0e0de655f74ebbb5ac0dd9f90b04855236cb
Art Navsegda <artnavsegda@gmail.com>
2019-10-31 16:31:38 +0300

    server java

--- library/sockets/server.java
+++ library/sockets/server.java
@@ -6,6 +6,8 @@ import java.io.OutputStream;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.Base64;
+import java.security.MessageDigest;
 
 public class server {
 	public static void main(String[] args) throws Exception
@@ -18,5 +20,17 @@ public class server {
     Scanner s = new Scanner(in, "UTF-8");
     String data = s.useDelimiter("\\r\\n\\r\\n").next();
 		Matcher get = Pattern.compile("^GET").matcher(data);
+    if (get.find())
+    {
+      Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
+      match.find();
+      byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n"
+          + "Connection: Upgrade\r\n"
+          + "Upgrade: websocket\r\n"
+          + "Sec-WebSocket-Accept: "
+          + Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1").digest((match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8")))
+          + "\r\n\r\n").getBytes("UTF-8");
+      out.write(response, 0, response.length);
+    }
 	}
 }
