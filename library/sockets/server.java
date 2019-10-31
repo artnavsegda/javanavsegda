c4d73d045effc55a4745dea02d9a3cb80c7e29bb
Art Navsegda <artnavsegda@gmail.com>
2019-10-31 16:21:06 +0300

    java regex

--- library/sockets/server.java
+++ library/sockets/server.java
@@ -4,6 +4,8 @@ import java.io.PrintWriter;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class server {
 	public static void main(String[] args) throws Exception
@@ -14,5 +16,7 @@ public class server {
     InputStream in = client.getInputStream();
     OutputStream out = client.getOutputStream();
     Scanner s = new Scanner(in, "UTF-8");
+    String data = s.useDelimiter("\\r\\n\\r\\n").next();
+		Matcher get = Pattern.compile("^GET").matcher(data);
 	}
 }
