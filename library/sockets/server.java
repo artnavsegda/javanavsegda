989e0184464cd3ff286f3193348fecc5e171e46e
Art Navsegda <artnavsegda@gmail.com>
2019-10-31 16:15:52 +0300

    jaba

--- library/sockets/server.java
+++ library/sockets/server.java
@@ -1,12 +1,18 @@
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
 
 public class server {
 	public static void main(String[] args) throws Exception
 	{
 		ServerSocket server = new ServerSocket(8888);
 		Socket client = server.accept();
-		PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+		//PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+    InputStream in = client.getInputStream();
+    OutputStream out = client.getOutputStream();
+    Scanner s = new Scanner(in, "UTF-8");
 	}
 }
